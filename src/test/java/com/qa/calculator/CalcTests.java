7bf320173d6c63235c3b24724db2ba564cff20fe
Sam Kirk <samueljkirk10@gmail.com>
2018-07-11 11:45:09 +0100

    test pass

---
+++ src/test/java/com/qa/calculator/CalcTests.java
@@ -0,0 +1,37 @@
+package com.qa.calculator;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CalcTests {
+	Add add;
+	Multiply multi;
+	Subtract sub;
+	
+	@Before
+	public void setUp() {
+		add = new Add();
+		multi = new Multiply();
+		sub = new Subtract();
+	}
+
+	@Test
+	public void addTest() {
+		int actualValue = add.add(2, 2);
+		Assert.assertEquals(4, actualValue);
+	}
+	
+	@Test
+	public void multiplyTest() {
+		int actualValue = multi.multiply(2, 3);
+		Assert.assertEquals(6, actualValue);
+	}
+	
+	@Test
+	public void subtractTest() {
+		int actualValue = sub.subtract(10, 5);
+		Assert.assertEquals(5, actualValue);
+	}
+
+}
